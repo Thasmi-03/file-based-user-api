3b5bb69a19708cf127b3e67a381868c37726c1df
Thasmiga <vimalthasmiga1112@gmail.com>
2025-10-06 19:11:50 +0530

    recheak all http method

--- app.js
+++ app.js
@@ -3,13 +3,9 @@ import bodyParser from "body-parser";
 import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
-
-// Middleware
 app.use(bodyParser.json());
 
-// Routes
 app.use("/api/users", userRoutes);
 
-// Start server
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
